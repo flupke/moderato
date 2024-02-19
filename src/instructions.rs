013703bcd1cdccbef81c254815a04e248f531254
Luper Rouch <luper.rouch@gmail.com>
2024-02-19 11:18:57 +0100

    More instruction types

--- src/instructions.rs
+++ src/instructions.rs
@@ -1,4 +1,4 @@
-use rand::seq::SliceRandom;
+use rand::seq::{IteratorRandom, SliceRandom};
 
 fn random_hand_instructions() -> String {
     let mut rng = rand::thread_rng();
@@ -9,7 +9,6 @@ fn random_hand_instructions() -> String {
             .choose(&mut rng)
             .unwrap()
     )
-    .to_string()
 }
 
 fn random_foot_instructions() -> String {
@@ -18,11 +17,39 @@ fn random_foot_instructions() -> String {
         "Lève ton pied {}",
         ["gauche", "droit"].choose(&mut rng).unwrap(),
     )
-    .to_string()
+}
+
+fn random_addition() -> String {
+    let mut rng = rand::thread_rng();
+    format!(
+        "{} + {} = ?",
+        (1..10).choose(&mut rng).unwrap(),
+        (1..10).choose(&mut rng).unwrap()
+    )
+}
+
+fn random_clothes_color_question() -> String {
+    let mut rng = rand::thread_rng();
+    format!(
+        "De quelle couleur {} ?",
+        [
+            "est ton tee-shirt",
+            "est ton pantalon",
+            "sont tes chaussettes",
+            "est ton slip",
+        ]
+        .choose(&mut rng)
+        .unwrap(),
+    )
 }
 
 pub fn random_instructions() -> String {
     let mut rng = rand::thread_rng();
-    let funcs = [random_hand_instructions, random_foot_instructions];
+    let funcs = [
+        random_hand_instructions,
+        random_foot_instructions,
+        random_addition,
+        random_clothes_color_question,
+    ];
     funcs.choose(&mut rng).unwrap()()
 }
